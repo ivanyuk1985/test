7c9e059f7e1b32ac369807774443e598c5624c37
ivanyuk1985 <101967008+ivanyuk1985@users.noreply.github.com>
2023-05-06 09:25:37 +0300

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo rm -f /var/www/html/index.html
-sudo cp /var/lib/jenkins/workspace/Apache/html/index.html /var/www/html
+rm -f /var/www/html/index.html
+cp /var/lib/jenkins/workspace/Apache/html/index.html /var/www/html
